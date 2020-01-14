858946a911a2cc92876eb3758fa816e8209554fe
Candace <2100978@talnet.nl>
2020-01-14 14:35:41 +0100

    Uitwerking van de exercise

---
+++ name.php
@@ -0,0 +1,10 @@
+<?php
+
+        $voornaam = readline("Wat is je voornaam?");
+        
+        $achternaam = readline("Wat is je achternaam?");
+        
+        $antwoord = readline("Jouw naam is: ".$voornaam.$achternaam);
+
+        echo($antwoord);
+?>
